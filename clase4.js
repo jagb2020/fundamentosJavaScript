ff2ed99c7138efd9e583731a256fff91d9ade298
Angel Gonzalez <agb92@live.com>
2020-10-30 23:54:09 -0600

    Funciones

---
+++ clase4.js
@@ -0,0 +1,15 @@
+var nombre = 'Angel', edad = 28
+
+// function imprimirEdad(){
+//     console.log(`${nombre} tiene ${edad} años`)
+// }
+
+// imprimirEdad()
+
+function imprimirEdad(n, e){
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad(edad, nombre)
+imprimirEdad()
