ca5dfb3688162f7c970180cb1dc3442e7b5514dc
Angel Gonzalez <agb92@live.com>
2020-10-31 11:11:21 -0600

    alcance de las funciones

---
+++ clase5.js
@@ -0,0 +1,25 @@
+var nombre = 'Angel'
+
+// function imprimirNombreEnMayusculas() {
+//     nombre = nombre.toUpperCase()
+//     console.log(nombre)
+// }
+
+// imprimirNombreEnMayusculas()
+// nombre //ANGEL
+
+// function imprimirNombreEnMayusculas(n) {
+//     n = n.toUpperCase()
+//     console.log(n)
+// }
+    
+// imprimirNombreEnMayusculas(nombre)
+// nombre //ANGEL
+
+function imprimirNombreEnMayusculas(nombre) {
+    nombre = nombre.toUpperCase()
+    console.log(nombre)
+}
+        
+imprimirNombreEnMayusculas(nombre)
+nombre //ANGEL
