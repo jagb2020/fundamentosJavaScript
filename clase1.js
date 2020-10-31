428111d47fe3d51d875c08dcb009351bdaec4b61
Angel Gonzalez <agb92@live.com>
2020-10-30 22:34:11 -0600

    Variables

---
+++ clase1.js
@@ -0,0 +1,17 @@
+/**
+ * hola mundo
+ */
+// console.log('hola mundo JS')
+
+/**
+ * variables
+ */
+var nombre
+nombre = 'Angel'
+
+var apellido = 'Gonzlez', sexo = 'masculino'
+
+var edad = 28
+
+console.log('Hola ' + nombre + ' ' + apellido + ' ' + sexo)
+console.log('Tengo ' + edad + ' años')
---
+++ readme.md
@@ -0,0 +1,2 @@
+Fundamentos de JavaScript 
+* Variables
