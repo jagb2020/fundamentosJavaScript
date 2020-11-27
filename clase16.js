3ed5378138694f9c73454b37986461c63ce83db2
Angel Gonzalez <agb92@live.com>
2020-11-26 20:51:35 -0600

    Condicional múltiple: switch

---
+++ clase16.js
@@ -0,0 +1,64 @@
+console.log(`clase 16`)
+
+var signo = prompt('¿Cuál es tu signo?')
+
+// console.log(signo)
+
+switch (signo ){
+    case 'aries':
+        console.log('Anímate, afortunada Aries, porque hoy puedes recibir un mensaje muy importante e inesperado sobre algo que te puede cambiar la existencia por completo a diario y que te hará muy feliz porque no pensabas que pudiera sucederte a ti. Y es que la vida tiene estas cosas, te recompensa cuando menos te lo esperas. Por eso mismo tienes que dejar de lado esas dudas que tienes acerca de tu pareja, porque te estás comparando con otras personas que han adquirido ya un mayor compromiso, que se van a vivir juntos o que hacen cosas que tú estás deseando también a diario. Todos tenemos un proceso personal diferente y ten por seguro que tú también tendrás todo lo que deseas, nativa de Aries, pero a su debido tiempo. No seas impaciente y hoy piensa en el fin de semana genial que te espera.')
+        break
+
+    case 'tauro':
+        console.log('Desde hace unos días, sociable Tauro, estás deseando acercarte más a tus compañeros de trabajo, tener más complicidad con quienes formas equipo a diario porque ahora te das cuenta de que esto tiene mucho valor tanto a nivel profesional como personal . Hoy es un día muy bueno para estrechar lazos fuera del ámbito de trabajo. Proponles tomar una copa juntos a la salida, charlar distendidamente de otros temas más personales. Será un paso muy importante y muy apreciado por ellos. Quizá alguno de ellos proponga otra idea para pasarlo bien juntos. Adáptate sin reticencias, déjate fluir, que la vida te lleve por el camino más indicado. Si tienes pareja, nativa de Tauro, hoy invítale a compartir ese encuentro, te ayudará a crear el clima cálido que necesitas para que esta jornada de convivencia dé sus frutos a diario.')
+        break
+
+    case 'cancer':
+    case 'cáncer':
+        console.log('Aunque tienes un pensamiento muy liberal y te parece muy bien que los demás hagan lo que les parezca, tolerante Cáncer, reconoce hoy que cuando se trata de ti misma te cuesta un poco soltarte. Tal vez temes salirte de los límites o ser criticada. Pasa de todo y vive como quieras, haz lo que desees a diario y ama de la forma que te dicta el corazón, No tengas en cuenta el qué dirán ni la opinión de gente. Necesitas comenzar a experimentar mucho más, dejarte llevar por el amor y vivir la vida tan a tope como puedas, prudente Cáncer. Este fin de semana que hoy comienza, procura salir y asistir a todo lo que te propongan. Si te queda algún rato libre, entonces organiza tú una salida de amigos. En estas conversaciones encontrarás muchas claves que te hacen falta, a diario.')
+        break  
+        
+    case 'geminis':
+    case 'géminis':
+        console.log('Tendrías que dedicar el día, incluso este fin de semana que hoy se inicia, nativa de Géminis, a recuperarte del esfuerzo que has hecho en el trabajo a diario. Te sientes presionada y estás hasta la peineta de tanto esfuerzo. Tranquilízate que se trata sólo de una etapa y que además todo lo que haces está siendo muy valorado, quizá estén pensando en un proyecto para ti que te va a entusiasmar. Si estás al inicio de una relación, no seas muy intensa, apasionada Géminis, no estés pendiente a diario de si quedáis, si te llama o si te pone mensajes. Espacia un poco las citas y no le atosigues intentando hablar con él a todas horas, despertarás más su interés. Hoy comparte tu tiempo con tus amigos y tu familia. Te ayudarán a pasar el bache.')
+        break
+    case 'leo':
+        console.log('Los astros protegen ahora los proyectos que emprendas, nativa de Leo, por eso si hoy una persona de tu entorno, quizá de la familia, te propone intervenir en un negocio, di que sí. No tienes que cambiar de trabajo, se trata de participar a diario en una buena idea que ha tenido. Acéptalo porque te puede reportar ingresos extra. Si sales hoy de compras procura elegir bien y no confíes en que puedes pagar lo que se te antoje, porque el dinero se va volando a diario, confiada Leo. Si ves que tienes un día de estos que la tarjeta de crédito va a echar humo, cambia el plan, no te acerques a la tentación. Mejor queda con tu pareja o con una amiga para pasear o para hacer un poco de ejercicio al anochecer. Mañana te alegrarás de haber tomado esta decisión.')
+        break
+    case 'virgo':
+        console.log('Aunque las cosas te estén yendo bien a diario, confiada Virgo, la vida no es plana y puntualmente tendrás que afrontar problemas que te servirán para crecer como persona, para ganar experiencia y hacerte más fuerte. Hoy tal vez suceda algo en el trabajo que te afecte directamente y que te cree una cierta frustración. Recupérate rápido porque no será más que un tropiezo sin importancia. Tienes inteligencia y capacidad de liderazgo, lo único que te falta es un poquitín de paciencia. La vida se ha de recorrer a pasos, cortos pero firmes. Tú de hecho ya estás en ello y lo sabes, avispada Virgo. Aprovecha hoy para buscar novedades sobre tu profesión, que es el tema que más te preocupa. Aprende todo lo que puedas a diario. Te será muy útil. Pero también guarda un espacio para el descanso y la diversión. Te conviene mucho.')
+        break
+    case 'libra':
+        console.log('Hoy, contrariada Libra, un contratiempo en el trabajo con el que no contabas para nada te hará poner de muy mal humor. A raíz de esto, alguien que está por encima de ti te hará comentario que te sentará fatal pero no has de tomarlo como algo personal, sino como un consejo. No te dejes vencer por el orgullo y sigue manteniendo con esta persona una relación cordial a diario porque te puede beneficiar mucho en el. futuro. En cambio, evita confiar esto que te ha pasado a algunos compañeros de esos que siempre están dispuestos a darte la palmada en la espalda. No compartas a diario tus problemas ni la información que tienes, confiada Libra, alguien de este grupo te tiene envidia. Sé correcta pero hoy no digas ni palabra de tus cosas o pueden utilizarlo en tu contra.')
+        break
+    case 'escorpion':
+        console.log('No te sientes bien hoy contigo misma porque estás indecisa y a la vez impaciente, nativa de Escorpio. Las cosas a diario no van tan deprisa como a ti te gustaría y hoy puede darte la ventolera de tirar por la calle del medio, aunque no veas claro el resultado que esto te dará. No es momento para arriesgarte, espera hasta tener una situación económica más segura. No pierdas la estabilidad que tienes en este momento, necesitas mantenerla. Tendrás hoy una buena noticia en el entorno familiar. Si tienes pareja, vuestra relación avanza a diario hacia un futuro feliz, ilusionada Sagitario. Si todavía estás libre, hoy una persona encantadora, se mostrará pendiente de ti. El problema es que ni te gusta ni quieres nada con esa persona. No le hagas perder el tiempo y procura conservarle como amigo.')
+        break
+    case 'sagitario':
+        console.log('Si tienes ganas de relacionarte socialmente y divertirte a diario, hoy tendrás ocasión de ello, nativa de Sagitario. Podría ser en alguna celebración que tenga lugar en tu empresa, o quizá te inviten a participar en un evento por sorpresa. En ambos casos la vida jugará a tu favor. Si cenas con los compañeros, será el momento de conoceros mejor y limar asperezas, y si lo haces con otra gente, tendrás oportunidad de enterarte de algunas cosas que te pueden resultar muy útiles dentro de poco tiempo. Una reunión familiar puede proporcionarte una información valiosa. O sea que hoy nada de quedarte en casa, sal, diviértete y ponte al corriente de lo que pasa por el mundo a diario, curiosa Sagitario. En el terreno amoroso, no te sientas mal si tu chico no puede acompañarte, mañana tendréis aún más ganas de veros')
+        break
+    case 'capricornio':
+        console.log('Haz acopio hoy de energía y piensa que ya tienes un pie en el fin de semana, estresada Capricornio. Aunque hoy te toque trabajar a tope y quizá un poco más allá de tu horario, te lo van a recompensar. Dentro de pocos días te alegrarás de no haber puesto peros a lo que toque hacer hoy. Y es que las cosas buenas, si además llegan por sorpresa, son todavía mejores.Te irá bien airearte un poco a diario este fin de semana. Si te sale la oportunidad de salir fuera de la ciudad uno de estos dos días o ambos, di que sí, porque te lo pasarás bien y tendrás ocasión de contactar con alguien que agradecerás a diario haber conocido. Además, ya va siendo hora de que salgas de la rutina con tu pareja. Esta salida os vendrá muy bien para reencontraros.')
+        break
+    case 'acuario':
+        console.log('Hoy, agobiada Acuario, procura pasar de todo lo que te está creando ansiedad a diario en los últimos días y piensa en serio en tomarte un buen descanso este fin de semana. Si es en el amor donde tienes los mayores problemas, descansa también de esto en soledad. Aléjate de los problemas y recupera la energía. Un hecho que te ha pasado muy recientemente ha hecho tambalear tu seguridad en ti misma, pero ya te estás recuperando y sucederá un acontecimiento en tu vida que te hará estar pronto en óptimas condiciones. No pienses que todo te sale mal, pesimista Acuario. Revisa hoy los montones de cosas positivas que has conseguido. Levanta el ánimo y piensa en las nuevas posibilidades que tienes ante ti a diario. Recuerda que las cosas, incluso las negativas, no pasan por casualidad.')
+        break
+    case 'piscis':
+        console.log('Ahora es un buen momento poner en práctica esta idea que te ronda por la cabeza a diario desde hace tiempo, creativa Piscis, ya no esperes más. Prepárate hoy y durante este fin de semana. Investiga el mercado en el que te mueves porque es súper importante que estés muy puesta en todo lo que se cuece en él, las novedades y la forma de publicitarlas. Sin embargo, no es el momento de dejar el trabajo que tienes, aunque te cueste un poco de esfuerzo, has de seguir ahí por un tiempo, hasta que tengas claro que tu proyecto va a fructificar. Hoy, que no estás del mejor humor, puede surgir una discusión con tu pareja, susceptible Piscis, pero va a terminar de una forma muy apasionada. No le pilles el gusto a discutir a diario, no siempre termina así y más vale no tentar a la suerte.')
+        break
+    default:
+        console.log('no es un signo zodiacal valido')
+        break
+}
+
+// if (signo === 'acuario'){
+//     console.log('abc...')
+// }
+
+// if (signo === 'sagitario'){
+//     console.log('abc...')
+// }
+
+// if (signo === 'leo'){
+//     console.log('abc...')
+// }
